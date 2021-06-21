aa62b3220c361ab938b9d83757b0aacbe9adc1ec
farahasha <farahasha93@yahoo.com>
2021-06-21 17:55:58 +0300

    editt

---
+++ app/app1.js
@@ -0,0 +1,31 @@
+let username = prompt('welcome for you,can i know your name?');
+alert("hello"+ username);
+console.log(username);
+
+let age = prompt('DO YOU THINK I AM 27?');
+switch (age.toUpperCase){
+    case 'YES':
+    alert("Unfortunately, it is the truth " );
+  break;
+  case 'NO': 
+  alert("I humbly know this");
+console.log(age);
+}
+
+let study = prompt('Do you think programming is suitable for me?');
+alert('I should become a programming fashionista');
+console.log(study);
+
+let user = prompt('You think we got caught in a trap?');
+alert('also me');
+console.log(user);
+
+
+let answer = prompt("Do you think I'm enjoying it ?");
+alert ('so much wallha');
+console.log(answer) ;
+
+document.write('welcom in my littel world');
+
+
+
