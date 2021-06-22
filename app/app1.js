ba801ecc8aa56ee2cb159912624a61f70a5d5f32
farahasha <farahasha93@yahoo.com>
2021-06-22 12:52:38 +0300

    yes no

--- app/app1.js
+++ app/app1.js
@@ -3,27 +3,37 @@ alert("hello"+ username);
 console.log(username);
 
 let age = prompt('DO YOU THINK I AM 27?');
-switch (age.toUpperCase){
-    case 'YES':
-    alert("Unfortunately, it is the truth " );
-  break;
-  case 'NO': 
-  alert("I humbly know this");
 console.log(age);
-}
+if (age .toLocaleLowerCase() == 'yes'){
+ alert("Unfortunately, it is the truth " );
+} else {
+  alert("I humbly know this");
+  }
 
 let study = prompt('Do you think programming is suitable for me?');
-alert('I should become a programming fashionista');
 console.log(study);
+if (study .toLocaleLowerCase() === 'yes') {
+  alert('I should become a programming fashionista');
+} else{
+  alert("Don't worry about it just your opinion")
+}
+
 
 let user = prompt('You think we got caught in a trap?');
-alert('also me');
 console.log(user);
-
+if (user .toLocaleLowerCase() === 'yes'|| 'no' ){
+alert('MAKE SURE NOT THINK');
+}
 
 let answer = prompt("Do you think I'm enjoying it ?");
-alert ('so much wallha');
 console.log(answer) ;
+if (user .toLocaleLowerCase() === 'no' ){
+alert('ITS OKY ,LIFE IS HARD')
+} else {
+  alert ('so much wallha');
+
+}
+
 
 document.write('welcom in my littel world');
 
